3b4ace445e3583e6cfa79fb069db4b286d4c8fbd
Alpar <szabadosalpar@gmail.com>
2022-05-21 20:21:21 +0300

    fixes

--- yt-playlist-down/app.py
+++ yt-playlist-down/app.py
@@ -1,37 +1,47 @@
 from subprocess import call
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
-from flask import request
+
+
+on = False
 
 
 def download_playlist():
-    file1 = open('./playlists.txt', 'r')
-    lines = file1.readlines()
+    global on
+    if not on:
+        try:
+            on = True
+            files = open('/data/playlists.txt', 'r')
+            lines = files.readlines()
 
-    count = 0
-    for line in lines:
-        count += 1
-        url = line.strip()
-        print("Processing playlist nr{}: {}".format(count, url))
-        call(["youtube-dl", url, "--audio-format", "mp3", "-x", "--yes-playlist", "--download-archive", "records.txt",
-              "-o", "/music/%(playlist)s/%(title)s.%(ext)s"])
-
+            count = 0
+            for line in lines:
+                count += 1
+                url = line.strip()
+                print("Processing playlist nr{}: {}".format(count, url))
+                call(["youtube-dl", url, "-f", "bestaudio", "--extract-audio", "--audio-format", "mp3", "-x",
+                      "--yes-playlist", "--download-archive", "/data/records.txt", "-o",
+                      "/music/%(playlist)s/%(title)s.%(ext)s", "--embed-thumbnail", "--add-metadata",
+                      "--postprocessor-args", "-id3v2_version 3"])
+                # youtube-dl https://www.youtube.com/playlist?list=PLv3TTBr1W_9tppikBxAE_G6qjWdBljBHJ -f bestaudio --extract-audio --audio-format mp3 -x --yes-playlist --download-archive records.txt -o '/%(playlist)s/%(title)s.%(ext)s' --embed-thumbnail --add-metadata --postprocessor-args '-id3v2_version 3'
+                on = False
+        except:
+            on = False
+            download_playlist()
+    else:
+        print("waiting")
 
 sched = BackgroundScheduler()
-sched.add_job(download_playlist, 'cron', month='*', day='*', hour='04', minute='*', max_instances=1)
+sched.add_job(download_playlist, 'cron', month='*', day='*', hour='*', minute='1, 15, 30, 45', max_instances=1)
 sched.start()
 
 app = Flask(__name__)
 
 
-@app.route('/download/')
+@app.route('/restart')
 def download():
-    url = request.args.get('url', default=1, type=str)
-    print(str(url))
-    call(["youtube-dl", url, "--audio-format", "mp3", "-x", "--yes-playlist", "--download-archive", "records.txt",
-          "-o", "/music/%(playlist)s/%(title)s.%(ext)s"])
-
-    return 'starting...'
+    download_playlist()
+    return 'restart...'
 
 
 if __name__ == "__main__":
